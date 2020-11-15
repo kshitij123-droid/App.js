4a3efefbf92741639b86985289bb48f79936cb27
kshitij123-droid <54199335+kshitij123-droid@users.noreply.github.com>
2020-11-15 09:09:50 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
-const mongoose = require('mongoose');
+const index = require('./routes/index')
+//const mongoose = require('mongoose');
 const expressEjsLayout = require('express-ejs-layouts')
 //mongoose
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
-.then(() => console.log('connected,,'))
-.catch((err)=> console.log(err));
+//mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
+//.then(() => console.log('connected,,'))
+//.catch((err)=> console.log(err));
 //EJS
 app.set('view engine','ejs');
 app.use(expressEjsLayout);
@@ -14,7 +15,14 @@ app.use(expressEjsLayout);
 app.use(express.urlencoded({extended : false}));
 
 //Routes
-app.use('/',require('./routes/index'));
+app.use('/',index);
+app.set('view engine','ejs');
+app.get('/',(req,res) => {
+    res.render('index');
+})
 app.use('/users',require('./routes/users'));
+app.get('/',(req,res) => {
+    res.render('users');
+})
 
 app.listen(3000); 
